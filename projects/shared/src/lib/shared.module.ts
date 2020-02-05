e1725ee38a7f2cbeb44dc559ea57ba2030969dea
Rob Dean <rob@inq.me>
2020-02-05 15:01:33 -0500

    wip

--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
+
 import { SharedComponent } from "./shared.component";
 import { UserListComponent } from "./user-list/user-list.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [SharedComponent, UserListComponent],
-  imports: [],
+  imports: [BrowserModule],
   exports: [SharedComponent, UserListComponent]
 })
 export class SharedModule {}
--- projects/shared/src/lib/user-list/user-list.component.sandbox.ts
+++ projects/shared/src/lib/user-list/user-list.component.sandbox.ts
@@ -1,6 +1,15 @@
 import { sandboxOf } from "angular-playground";
 import { UserListComponent } from "./user-list.component";
 
-export default sandboxOf(UserListComponent).add("default", {
-  template: `<lib-user-list label="Uzerz"></lib-user-list>`
-});
+export default sandboxOf(UserListComponent, {
+  label: "feature 1"
+})
+  .add("default", {
+    template: `<lib-user-list label="Uzerz"></lib-user-list>`
+  })
+  .add("with one user", {
+    template: `<lib-user-list label="One User List" [users]="oneUserList"></lib-user-list>`,
+    context:{
+      oneUserList = [{name: 'Test User'}]
+    }
+  });
