04f5b7cb34e4ef356d38b2cdb6b32e6692717ccc
Rob Dean <rob@inq.me>
2020-02-06 12:16:33 -0500

    adding scenarios to sandbox

--- projects/shared/src/lib/user-list/user-list.component.sandbox.ts
+++ projects/shared/src/lib/user-list/user-list.component.sandbox.ts
@@ -5,11 +5,23 @@ export default sandboxOf(UserListComponent, {
   label: "feature 1"
 })
   .add("default", {
-    template: `<lib-user-list label="Uzerz"></lib-user-list>`
+    template: `<lib-user-list label="List with no users"></lib-user-list>`
   })
   .add("with one user", {
     template: `<lib-user-list label="One User List" [users]="oneUserList"></lib-user-list>`,
-    context:{
-      oneUserList = [{name: 'Test User'}]
+    context: {
+      oneUserList: [{ name: "Test User" }]
+    }
+  })
+  .add("with five users", {
+    template: `<lib-user-list label="One User List" [users]="oneUserList"></lib-user-list>`,
+    context: {
+      oneUserList: [
+        { name: "Test User" },
+        { name: "Test User" },
+        { name: "Test User" },
+        { name: "Test User" },
+        { name: "Test User" }
+      ]
     }
   });
--- projects/shared/src/lib/user-list/user-list.component.ts
+++ projects/shared/src/lib/user-list/user-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class UserListComponent implements OnInit {
   constructor() {}
-  @Input() Users: any[] = [];
+  @Input() users: any[] = [];
   @Input() label = "User List!";
   ngOnInit() {}
 }
